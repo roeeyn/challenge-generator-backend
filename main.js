ff555f4983db7109826edcb20e3b34bfb66d4f60
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-07-29 19:12:16 -0500

    Added starter structure for running express

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("holi");
+const app = require("express")();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Todo al 100 en el server"));
+
+app.listen(PORT, () => console.log(`Server running at ${PORT}`));
