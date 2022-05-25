7f341509c7152da923b07ace117f41831fbd8004
Rodrigo Medina <rodrigom@lyft.com>
2022-05-24 21:36:13 -0500

    Added ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule ChallengeGenerator.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:mongodb_driver, "~> 0.9.0"}
+      {:mongodb_driver, "~> 0.9.0"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
     ]
   end
 
